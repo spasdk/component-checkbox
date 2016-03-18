f49350ca7ab05212e85e2377e11506e4a8d597c8
Stanislav Kalashnik <s.kalashnik@infomir.com>
2016-03-18 18:43:51 +0200

    start reworking develop checks

--- index.js
+++ index.js
@@ -32,6 +32,8 @@ function CheckBox ( config ) {
     // sanitize
     config = config || {};
 
+    console.assert(typeof this === 'object', 'must be constructed via new');
+    
     if ( DEVELOP ) {
         if ( typeof config !== 'object' ) { throw new Error(__filename + ': wrong config type'); }
         // init parameters checks
